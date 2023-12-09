615a89a1fef7ef25b4459eb9d4c0fa342f5c64fa
Stefan <trfstephan@gmail.com>
2023-12-09 16:39:52 +0100

    Pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -12,7 +12,7 @@ const especiales = ['A', 'J', 'Q', 'K'] // Creamos las cartas especiales
 
 // Función para crear y mezclar la baraja
 const crearDeck = () => {
-    for(let i = 2; i < 10; i++){     // Ejecutamos, por cada número del 2 al 10
+    for(let i = 2; i <= 10; i++){     // Ejecutamos, por cada número del 2 al 10
         for(let tipo of tipos){      // Ejecutamos, por cada elemento del arreglo tipos
             deck.push(i + tipo)          
         }
@@ -22,10 +22,29 @@ const crearDeck = () => {
             deck.push(esp + tipo)    
         }
     }
-    console.log(deck) 
     deck = _.shuffle(deck)
     console.log(deck) 
     return deck
 }
-// Llamamos a la función
 crearDeck()
+
+// Esta función me permite tomar una carta de la baraja
+const pedirCarta = () => {
+
+    // Comprobamos si hay cartas en el arreglo
+    if (deck.length === 0){
+        // Mensaje de error
+        throw 'No hay cartas en el deck'
+    }
+
+    // Removemos la última carta de la baraja
+    const carta = deck.pop()
+
+    console.log(deck) 
+    console.log(carta) // carta debe ser de la baraja
+    return carta
+}
+pedirCarta()    
+
+
+
